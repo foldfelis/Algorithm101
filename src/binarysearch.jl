82c32454611cc3a386d7d4ef0d3a6c19e5521e7c
JingYu Ning <foldfelis@gmail.com>
2020-02-05 20:02:26 +0800

    Implement binary search (#7)
    * Implement binary search
    
    * Implement in iteration way
    
    * refactor
    
    * correct missing return value -1

---
+++ src/binarysearch.jl
@@ -0,0 +1,17 @@
+function binary_search(array::Vector{T}, target::T) where T
+    left = 1
+    right = length(array)
+
+    while left <= right
+        mid = trunc(Int, left+(right-left)/2)
+        if array[mid] > target
+            right = mid-1
+        elseif array[mid] < target
+            left = mid+1
+        else
+            return mid
+        end
+    end
+
+    return -1
+end
