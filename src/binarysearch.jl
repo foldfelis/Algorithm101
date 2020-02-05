a266b8f04331ed229c2b204ba0e059df2aa923e0
JingYu Ning <foldfelis@gmail.com>
2020-02-05 19:52:41 +0800

    refactor

--- src/binarysearch.jl
+++ src/binarysearch.jl
@@ -1,24 +1,15 @@
-function binary_search(
-    array::Vector{T}, target::T) where T
-
+function binary_search(array::Vector{T}, target::T) where T
     left = 1
     right = length(array)
 
     while left <= right
-        mid = trunc(Int, (left+right)/2)
+        mid = trunc(Int, left+(right-left)/2)
         if array[mid] > target
-            right = mid
+            right = mid-1
         elseif array[mid] < target
-            left = mid
-        elseif array[mid] == target
+            left = mid+1
+        else
             return mid
         end
-
-        # when left == mid, and didn't return,
-        # the target is eather at the right boundary or not in the array
-        # Therefore, if not at the right boundary, target is not in the array
-        if left == mid && array[mid+1] > target break end
     end
-
-    return -1
 end
