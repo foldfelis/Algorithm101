bb08afa5984b2d5a0b6b0a4ba26c47c40871bcb7
JingYu Ning <foldfelis@gmail.com>
2020-02-14 16:42:30 +0800

    refactor test

--- test/binarysearch.jl
+++ test/binarysearch.jl
@@ -1,19 +1,14 @@
-@info "Test: 1:7:1000 sorted array"
-array = collect(1:7:1000)
-println(array, "\n")
+@testset "Test QuickSort" begin
 
-target_i = rand(1:length(array))
-target = array[target_i]
-@info "Target: $(target), at index: $(target_i)"
+    # Test: 1:7:1000 sorted array
+    array = collect(1:7:1000)
+    target_i = rand(1:length(array))
+    target = array[target_i]
+    @test binary_search(array, target) == target_i
 
-find_i = binary_search(array, target)
-@info "Found target at index: $(find_i)"
-println(@test target_i == find_i)
-println()
+    # Test: If element is not in the array
+    target = 6
+    find_i =
+    @test binary_search(array, target) == -1
 
-@info "Test: If element is not in the array"
-target = 6
-@info "Target: $(target), and is not in the array"
-find_i = binary_search(array, target)
-@info "Can't find and return index: $(find_i)"
-println(@test -1 == find_i)
+end
--- test/mergesort.jl
+++ test/mergesort.jl
@@ -1,15 +1,17 @@
-@info "Test: 1 elements Vector"
-answer = [0]
-print(@test merge_sort([0]) == answer)
-println(" : $(answer)\n")
+@testset "Test QuickSort" begin
 
-@info "Test: 7 elements Vector"
-answer = [1,2,3,4,5,6,7]
-print(@test merge_sort([4,6,5,7,1,3,2]) == answer)
-println(" : $(answer)\n")
+    # Test: 1 elements Vector
+    answer = [0]
+    @test merge_sort([0]) == answer
 
-@info "Test: 100 elements Vector"
-data = rand(100)
-answer = sort(data)
-print(@test merge_sort(data) == answer)
-println(" : $(answer)\n")
+    # Test: 7 elements Vector
+    data = [4,6,5,7,1,3,2]
+    answer = sort(data)
+    @test merge_sort(data) == answer
+
+    # Test: 100 elements Vector
+    data = rand(100)
+    answer = sort(data)
+    @test merge_sort(data) == answer
+
+end
