99973ba3354af8a18204392a8714ee233f757c93
JingYu Ning <foldfelis@gmail.com>
2020-03-14 14:57:55 +0800

    refactor

--- src/dfs.jl
+++ src/dfs.jl
@@ -1,10 +1,12 @@
-function dfs(g::Graph, v::Int64, traversed::Vector{Int64}=Int[])
+function dfs!(g::Graph, v::Int64, traversed::Vector{Int64}=Int[])
     push!(traversed, v)
 
     neighbors = neighbor(g, v)
     for n in neighbors
-        !(n in traversed) && dfs(g, n, traversed)
+        !(n in traversed) && dfs!(g, n, traversed)
     end
 
     return traversed
 end
+
+dfs(g::Graph, v::Int64) = dfs!(g, v)
