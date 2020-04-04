0eddc07b37d35bd427f8b31bdc07c63ef0037d19
JingYu Ning <foldfelis@gmail.com>
2020-04-04 15:05:35 +0800

    dfs

--- test/dfs.jl
+++ test/dfs.jl
@@ -1,23 +1,26 @@
+using DataStructure101
+const DS = DataStructure101
+
 @testset "DFS" begin
 
-    g = AdjacencyMatrix(10)
+    g = DS.AdjacencyMatrix(10)
 
-    relate!(g, 10, 9)
-    relate!(g, 10, 8)
+    DS.relate!(g, 10, 9)
+    DS.relate!(g, 10, 8)
 
-    relate!(g, 9, 7)
-    relate!(g, 9, 6)
-    relate!(g, 8, 5)
-    relate!(g, 8, 4)
+    DS.relate!(g, 9, 7)
+    DS.relate!(g, 9, 6)
+    DS.relate!(g, 8, 5)
+    DS.relate!(g, 8, 4)
 
-    relate!(g, 7, 3)
-    relate!(g, 7, 2)
-    relate!(g, 6, 1)
+    DS.relate!(g, 7, 3)
+    DS.relate!(g, 7, 2)
+    DS.relate!(g, 6, 1)
 
     @test dfs(g, 10) == [10, 8, 4, 5, 9, 6, 1, 7, 2, 3]
 
     n = 100
-    city = WeightedAdjacencyMatrix{Float64}(n=n, random_g=true)
+    city = DS.WeightedAdjacencyMatrix{Float64}(n=n, random_g=true)
     @test dfs(city, 1) == collect(1:100)
 
 end
