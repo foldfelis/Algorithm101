d296db35fd122abf03667bc132105d07cd9ecdcd
JingYu Ning <foldfelis@gmail.com>
2020-04-09 18:40:51 +0800

    quick sort

--- test/quick_sort.jl
+++ test/quick_sort.jl
@@ -1,13 +1,17 @@
+using Test
+using Algorithm101
+const ALGO = Algorithm101
+
 @testset "Test QuickSort" begin
 
     # Test: 5 elements
     data = [5,2,4,3,1]
     answer = sort(data)
-    @test quick_sort!(data, 1, length(data)) == answer
+    @test ALGO.quick_sort!(data, 1, length(data)) == answer
 
     # Test: 100 elements
     data = rand(100)
     answer = sort(data)
-    @test quick_sort!(data, 1, length(data)) == answer
+    @test ALGO.quick_sort!(data, 1, length(data)) == answer
 
 end
