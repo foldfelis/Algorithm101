2a6252884b673038d57ba4be61a8c0ced4dfded1
JingYu Ning <foldfelis@gmail.com>
2020-04-09 21:09:16 +0800

    Implement BST

--- src/binary_search_tree.jl
+++ src/binary_search_tree.jl
@@ -1,50 +1,74 @@
-using DataStructure101
-const DS = DataStructure101
-const ALGO = Algorithm101
+export BinarySearchTreeNode, root, key, value
 
-export find, bst_maximum, bst_minimum, get_bst_index
+abstract type AbstractBinarySearchTreeNode end
 
-key(tn::DS.TreeNode) = tn.index
+struct NullBinarySearchTreeNode <: AbstractBinarySearchTreeNode end
 
-find(tn::DS.NullNode, k::Int64) = nothing
+mutable struct BinarySearchTreeNode{T} <: AbstractBinarySearchTreeNode
+    data::Pair{Int64, T}
+    parent::AbstractBinarySearchTreeNode
+    left::AbstractBinarySearchTreeNode
+    right::AbstractBinarySearchTreeNode
 
-function find(tn::DS.TreeNode{T}, k::Int64) where T
+    BinarySearchTreeNode{T}(data::Pair{Int64, T}) where T =
+        new(
+            data, 
+            NullBinarySearchTreeNode(), 
+            NullBinarySearchTreeNode(), 
+            NullBinarySearchTreeNode()
+        )
+end
+
+key(tn::BinarySearchTreeNode) = tn.data.first
+
+value(tn::BinarySearchTreeNode{T}) where {T} = tn.data.second
+
+function find(tn::BinarySearchTreeNode{T}, k::Int64) where T
     if k == key(tn)
         return tn
     elseif k < key(tn)
-        left = DS.left_child(tn)
+        left = tn.left
         find(left, k)
     else
-        right = DS.right_child(tn)
+        right = tn.right
         find(right, k)
     end
 end
 
-function close_to(tn::DS.TreeNode{T}, k::Int64) where T
+find(tn::NullBinarySearchTreeNode, k::Int64) = nothing
+
+function root(tn::BinarySearchTreeNode)
+    parent = tn.parent
+    if !(parent isa NullBinarySearchTreeNode) tn = root(parent) end
+    
+    return tn
+end
+
+function close_to(tn::BinarySearchTreeNode{T}, k::Int64) where T
     if k <= key(tn)
-        left = DS.left_child(tn)
-        if left isa DS.NullNode return tn end
+        left = tn.left
+        if left isa NullBinarySearchTreeNode return tn end
         close_to(left, k)
     else
-        right = DS.right_child(tn)
-        if right isa DS.NullNode return tn end
+        right = tn.right
+        if right isa NullBinarySearchTreeNode return tn end
         close_to(right, k)
     end
 end
 
-function bst_maximum(bt::DS.BinaryTree)
-    tn = close_to(DS.root(bt), typemax(Int64))
-    return key(tn) => DS.value(tn)
+function Base.maximum(tn::BinarySearchTreeNode)
+    tn = close_to(root(tn), typemax(Int64))
+    return tn.data
 end
 
-function bst_minimum(bt::DS.BinaryTree)
-    tn = close_to(DS.root(bt), typemin(Int64))
-    return key(tn) => DS.value(tn)
+function Base.minimum(tn::BinarySearchTreeNode)
+    tn = close_to(root(tn), typemin(Int64))
+    return tn.data
 end
 
-function Base.insert!(bt::DS.BinaryTree{T}, data::Pair{Int64,T}) where T
-    parent = close_to(DS.root(bt), data.first)
-    tn = DS.TreeNode{T}(data.second, data.first)
+function Base.insert!(tn::BinarySearchTreeNode{T}, data::Pair{Int64,T}) where T
+    parent = close_to(root(tn), data.first)
+    tn = BinarySearchTreeNode{T}(data)
     tn.parent = parent
 
     if data.first < key(parent)
@@ -54,5 +78,4 @@ function Base.insert!(bt::DS.BinaryTree{T}, data::Pair{Int64,T}) where T
     end
 end
 
-get_bst_index(bt::DS.BinaryTree{T}, k::Int) where T = DS.value(ALGO.find(DS.root(bt), k))
-get_bst_index(bt::DS.NullNode, k::Int) where T = ALGO.find(bt, k)
+Base.getindex(tn::BinarySearchTreeNode{T}, k::Int) where T = value(find(root(tn), k))
--- test/binary_search_tree.jl
+++ test/binary_search_tree.jl
@@ -4,12 +4,28 @@ using Test
 using Algorithm101
 const ALGO = Algorithm101
 
-using DataStructure101
-const DS = DataStructure101
-
 @testset "Binary Search Tree" begin
 
-    data = Dict(
+    tn = ALGO.BinarySearchTreeNode{String}(11=>"Demo")
+    @test tn isa ALGO.BinarySearchTreeNode
+
+    @test ALGO.key(tn) == 11
+    @test ALGO.value(tn) == "Demo"
+
+    @test ALGO.find(tn, 10) == nothing
+    @test ALGO.find(tn, 11) == tn
+    @test ALGO.find(tn, 12) == nothing
+
+    @test ALGO.root(tn) == tn
+
+    @test maximum(tn) == tn.data
+    @test minimum(tn) == tn.data
+
+end
+
+@testset "Binary Search Tree Searching" begin
+
+    data = [
         1 => "Chinese",
         2 => "Hindi",
         3 => "Spanish",
@@ -20,21 +36,19 @@ const DS = DataStructure101
         8 => "Russian",
         9 => "Japanese",
         10 => "German",
-    )
+    ]
     shuffled_index = Random.shuffle(1:10)
     # shuffled_index = [3,5,1,8,4,9,2,10,6,7]
 
-    bt = DS.BinaryTree{String}(data[shuffled_index[1]], shuffled_index[1])
+    tn = ALGO.BinarySearchTreeNode{String}(data[shuffled_index[1]])
     for index in shuffled_index[2:end]
-        insert!(bt, index => data[index])
+        insert!(tn, data[index])
     end
 
-    @test ALGO.bst_maximum(bt) == (10 => "German")
-    @test ALGO.bst_minimum(bt) == (1 => "Chinese")
+    @test maximum(tn) == data[end]
+    @test minimum(tn) == data[1]
     for i in shuffled_index
-        @test ALGO.get_bst_index(bt, i) == data[i]
+        @test tn[i] == data[i].second
     end
-
-    @test ALGO.get_bst_index(DS.NullNode(), 11) == nothing
 
 end
