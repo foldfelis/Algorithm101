ea04892eea5e742a979888e67a368de26c5d953f
JingYu Ning <foldfelis@gmail.com>
2020-04-09 18:50:01 +0800

    tree traverse

--- test/tree_traverse.jl
+++ test/tree_traverse.jl
@@ -1,3 +1,8 @@
+using Test
+
+using Algorithm101
+const ALGO = Algorithm101
+
 using DataStructure101
 const DS = DataStructure101
 
@@ -9,8 +14,8 @@ const DS = DataStructure101
     for i=2:10
         push!(bt, i)
     end
-    @test pre_order(DS.root(bt)) == [1,2,4,8,9,5,10,3,6,7]
-    @test in_order(DS.root(bt)) == [8,4,9,2,10,5,1,6,3,7]
-    @test pos_order(DS.root(bt)) == [8,9,4,10,5,2,6,7,3,1]
+    @test ALGO.pre_order(DS.root(bt)) == [1,2,4,8,9,5,10,3,6,7]
+    @test ALGO.in_order(DS.root(bt)) == [8,4,9,2,10,5,1,6,3,7]
+    @test ALGO.pos_order(DS.root(bt)) == [8,9,4,10,5,2,6,7,3,1]
 
 end
